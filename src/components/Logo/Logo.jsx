4b05fa443c5bcd6762b44144f5d73d8306a67999
Ajay Balachandran <ajayxiia@gmail.com>
2021-05-27 21:38:00 +0800

    Removed unwanted class from logo

--- src/components/Logo/Logo.jsx
+++ src/components/Logo/Logo.jsx
@@ -5,7 +5,7 @@ import * as styles from './styles.module.css';
 
 export default () => (
   <div className={styles.wrapper}>
-    <StaticImage src="../../images/logo-icon.png" className={styles.logo} alt="Logo" />
+    <StaticImage src="../../images/logo-icon.png" alt="Logo" />
     <div className={styles.textContainer}>
       <b>AJAY</b>
       {' '}
